00ed21db53fac42e0f7c6cef8fff3afc4b39c35d
Nigel Baillie <metreckk@gmail.com>
2022-11-09 12:05:13 +0900

    Flesh out some komoju api types

--- src/komoju-fields-element.ts
+++ src/komoju-fields-element.ts
@@ -5,7 +5,33 @@ export default class KomojuFieldsElement extends HTMLElement {
     return [
       'session-id',
       'publishable-key',
+      'komoju',
     ];
+  }
+
+  session: KomojuSession | null = null
+
+  get komoju() {
+    const value = this.getAttribute('komoju');
+    if (!value || value === '') return 'https://komoju.com';
+    else return value;
+  }
+  set komoju(value) {
+    this.setAttribute('komoju', value ?? '');
+  }
+
+  get sessionId() {
+    return this.getAttribute('session-id');
+  }
+  set sessionId(value) {
+    this.setAttribute('session-id', value ?? '');
+  }
+
+  get publishableKey() {
+    return this.getAttribute('publishable-key');
+  }
+  set publishableKey(value) {
+    this.setAttribute('publishable-key', value ?? '');
   }
 
   /*
@@ -16,4 +42,35 @@ export default class KomojuFieldsElement extends HTMLElement {
 
     Also we need to figure out how to configure both komoju.com and multipay.komoju.com endpoints.
   */
+
+  attributeChangedCallback(name: string, _oldValue: any, newValue: any) {
+    if (name === 'session-id' && typeof newValue === 'string' && newValue != '') {
+      // Just in case publishable-key is set before session-id, we fetch the session on next tick.
+      setTimeout(async () => {
+        const response = await this.komojuFetch('GET', `/api/v1/sessions/${newValue}`);
+
+        if (response.status === 404) {
+          console.error('Invalid KOMOJU session', newValue);
+          return;
+        }
+
+        if (response.status !== 200) {
+          console.error('Failed to retrieve KOMOJU session', response);
+          return;
+        }
+
+        this.session = await response.json();
+        // TODO: maybe call a render function here?
+      }, 0);
+    }
+  }
+
+  private komojuFetch(method: 'GET' | 'POST', path: string): Promise<Response> {
+    return fetch(`${this.komoju}${path}`, {
+      method,
+      headers: {
+        authorization: `Basic ${btoa(`${this.publishableKey}:`)}`
+      }
+    });
+  }
 }
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,37 @@
+interface KomojuKonbiniBrand {
+  icon: string,
+}
+
+interface KomojuKonbiniPaymentMethod extends KomojuPaymentMethod {
+  type: 'konbini',
+  brands: { [index: string]: KomojuKonbiniBrand }
+}
+
+interface KomojuPaymentMethod {
+  type: string,
+  customer_fee?: number,
+  amount?: number,
+  currency?: string,
+  exchange_rate?: number,
+  offsite?: boolean,
+  additional_fields?: [string]
+}
+
+interface KomojuSession {
+  id: string,
+  resource: 'session',
+  mode: 'payment' | 'customer' | 'customer_payment',
+  amount: number,
+  currency: string,
+  session_url: string,
+  return_url: string,
+  default_locale: string,
+  created_at: string,
+  cancelled_at: string,
+  completed_at: string,
+  status: 'pending' | 'completed'  | 'cancelled',
+}
+
 declare module '*.html' {
   const value: string;
   export default value;
