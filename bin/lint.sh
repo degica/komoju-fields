ab5d560a30526eaa4d4578f40cd23803dc30c97c
Nigel Baillie <metreckk@gmail.com>
2022-11-09 11:40:39 +0900

    lint output

--- bin/lint.sh
+++ bin/lint.sh
@@ -10,10 +10,10 @@ TSC="npx tsc --strict --noEmit --target es2020 --lib es2020,dom --module esnext"
 
 # Lint the main bundle (see bin/build.sh for more info on why we have separate bundles like this)
 $TSC src/index.ts
-echo src/index.ts OK
+echo BUNDLE: src/index.ts OK
 
 # Lint the individual fields
 for module in $(ls src/fields/*/module.ts); do
   $TSC "$module"
-  echo "$module" OK
+  echo BUNDLE: "$module" OK
 done
