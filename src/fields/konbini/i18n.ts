6a24a46a0010fdd36ace5bed8ed9c6bd2fde54a1
Nigel Baillie <metreckk@gmail.com>
2022-11-16 17:50:30 +0900

    Add konbini module

---
+++ src/fields/konbini/i18n.ts
@@ -0,0 +1,23 @@
+export const en = {
+  'kb.label.name': 'Name (shown on receipt)',
+  'kb.label.email': 'Email address',
+  'kb.error.required': 'Required',
+  'kb.store.daily-yamazaki': 'Daily Yamazaki',
+  'kb.store.family-mart': 'FamilyMart',
+  'kb.store.lawson': 'Lawson',
+  'kb.store.ministop': 'Ministop',
+  'kb.store.seicomart': 'Seicomart',
+  'kb.store.seven-eleven': 'Seven Eleven',
+};
+
+export const ja: typeof en = {
+  'kb.label.name': '氏名（レシートで表示されます）',
+  'kb.label.email': 'メールアドレス',
+  'kb.error.required': '必須項目です',
+  'kb.store.daily-yamazaki': 'デイリーヤマザキ',
+  'kb.store.family-mart': 'ファミリーマート',
+  'kb.store.lawson': 'ローソン',
+  'kb.store.ministop': 'ミニストップ',
+  'kb.store.seicomart': 'セイコーマート',
+  'kb.store.seven-eleven': 'セブンイレブン',
+};
---
+++ src/fields/konbini/module.ts
@@ -0,0 +1,58 @@
+import '../../types.d'
+import html from './template.html'
+import { addValidation } from '../../shared/validation';
+
+import { registerMessages } from '../../shared/translations';
+import * as i18n from './i18n';
+registerMessages(i18n);
+
+export const render: KomojuRenderFunction = (root, paymentMethod) => {
+  root.innerHTML = html;
+  initializeInputs(root, root.host as KomojuFieldsConfig, paymentMethod as KomojuKonbiniPaymentMethod);
+}
+
+export const paymentDetails: KomojuPaymentDetailsFunction = (root, _paymentMethod) => {
+  const name = root.getElementById('kb-name')! as HTMLInputElement;
+  const email = root.getElementById('kb-email')! as HTMLInputElement;
+  const store = root.querySelector('input[name="kb-store"]:checked')! as HTMLInputElement;
+
+  return {
+    type: 'konbini',
+    store: store.value,
+    email: email.value,
+    name: name.value,
+  }
+}
+
+function initializeInputs(
+  document: DocumentFragment,
+  config: KomojuFieldsConfig,
+  paymentMethod: KomojuKonbiniPaymentMethod
+) {
+  const radioTemplate = document.getElementById('konbini-radio')! as HTMLTemplateElement;
+  const email = document.getElementById('kb-email')! as HTMLInputElement;
+  
+  let checked = false;
+  for (const brand in paymentMethod.brands) {
+    const element = radioTemplate.content.cloneNode(true) as HTMLElement;
+    const input = element.querySelector('input') as HTMLInputElement;
+    const image = element.querySelector('img') as HTMLImageElement;
+    const label = element.querySelector('komoju-i18n') as KomojuI18nElement;
+
+    input.value = brand;
+    if (!checked) {
+      input.checked = true;
+      checked = true;
+    }
+
+    image.src = `${config.komojuApi}${paymentMethod.brands[brand].icon}`;
+    label.key = `kb.store.${brand}`;
+
+    radioTemplate.parentElement!.appendChild(element);
+  }
+
+  addValidation(i18n, email, (input) => {
+    if (input.value === '') return 'kb.error.required';
+    return null;
+  });
+}
