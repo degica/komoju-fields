93a9ae9223770cda3d8a04b459b05efd5d2ed47f
Nigel Baillie <metreckk@gmail.com>
2022-11-29 18:03:29 +0900

    Add offsite spec

---
+++ cypress/e2e/offsite.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="cypress" />
+
+describe('KOMOJU Fields: Offsites', () => {
+  it('renders customer name for Alipay', () => {
+    cy.visit('/type/alipay');
+
+    cy.get('komoju-fields').shadow().find('#offsite-name').type('John Doe');
+
+    cy.contains('Pay').click();
+
+    cy.contains('KOMOJU Offsite Test Page').should('exist');
+    cy.contains('Capture').click();
+
+    cy.contains('Thanks for your payment!').should('exist');
+    cy.contains('Payment status: captured').should('exist');
+  });
+
+  it('renders no fields for PayPay', () => {
+    cy.visit('/type/paypay');
+
+    cy.wait(100);
+    cy.get('komoju-fields').shadow().find('input').should('not.exist');
+
+    cy.contains('Pay').click();
+
+    cy.contains('KOMOJU Offsite Test Page').should('exist');
+    cy.contains('Capture').click();
+
+    cy.contains('Thanks for your payment!').should('exist');
+    cy.contains('Payment status: captured').should('exist');
+  });
+});
