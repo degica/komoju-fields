1dfc16dffb9567dd9276aa7f122a03fcfd0e9158
Nigel Baillie <metreckk@gmail.com>
2022-12-06 15:38:47 +0900

    add source maps to production build

--- bin/build.sh
+++ bin/build.sh
@@ -13,7 +13,7 @@ ESBUILD_ARGS="--bundle --target=firefox100 --format=esm --loader:.html=text"
 
 # Pass --production to minify the output
 if [ "$1" = "--production" ]; then
-  ESBUILD_ARGS="$ESBUILD_ARGS --minify"
+  ESBUILD_ARGS="$ESBUILD_ARGS --minify --sourcemap=external"
   export ENV=production
 fi
 
