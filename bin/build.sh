d6c906266e54ac896cdf8c6d0b63e9d65eee5aab
Nigel Baillie <metreckk@gmail.com>
2022-12-07 11:43:41 +0900

    woops

--- bin/build.sh
+++ bin/build.sh
@@ -14,7 +14,7 @@ ESBUILD_ARGS="--bundle --target=firefox100 --format=esm --loader:.html=text"
 # Pass --production to minify the output
 if [ "$1" = "--production" ]; then
   ESBUILD_ARGS="$ESBUILD_ARGS --minify --sourcemap=external"
-  export ENV={ENV:-"production"}
+  export ENV={$ENV:-"production"}
 fi
 
 rm -rf dist
